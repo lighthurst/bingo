5e54d8da59a0ea3f59ee2387b7bae032d2df0d8b
Aaron Tesfai <aaron.tesfai@gmail.com>
2018-09-03 11:16:43 -0700

    Set up skeleton controller helper methods for route handling.

---
+++ server/controller/index.js
@@ -0,0 +1,14 @@
+// const model = require('../model');
+
+module.exports = {
+  drawBall: {
+    get(req, res) {
+      res.sendStatus(200);
+    },
+  },
+  verifyTicket: {
+    post(req, res) {
+      res.sendStatus(500);
+    },
+  },
+};
--- server/index.js
+++ server/index.js
@@ -1,11 +1,17 @@
-const express = require('express');
-const path = require('path');
 require('dotenv').config();
-
-const app = express();
 const PORT = process.env.PORT || 3001;
+const path = require('path');
 const PUBLIC_DIR = path.join(__dirname, '../public');
 
-app.use(express.json())
+const app = require('express')();
+
+app
+  // Parse JSON payloads
+  .use(express.json())
+  // Server public files
   .use(express.static(PUBLIC_DIR))
+  // 
+  .use('/gm', routes)
+  // Error handling
+  // .use(errorHandler)
   .listen(PORT, () => console.log(`Server listening on port ${PORT}`));
