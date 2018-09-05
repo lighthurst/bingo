0bcae2579f5ca4e22d2a4cc54daea46fbee5c244
Aaron Tesfai <aaron.tesfai@gmail.com>
2018-09-05 00:03:51 -0700

    Restyle component.

--- client/src/components/TicketDisplay.jsx
+++ client/src/components/TicketDisplay.jsx
@@ -3,19 +3,20 @@ import PropTypes from 'prop-types';
 import Ticket from './Ticket';
 
 const divStyle = {
-  height: '100%',
-  width: '100%',
-  maxWidth: '1000px',
+  alignItems: 'center',
   background: 'rgba(238, 242, 245, 1)',
   display: 'flex',
   flexWrap: 'wrap',
+  height: '88%',
   justifyContent: 'center',
   margin: '0 auto',
-  alignItems: 'center',
+  maxWidth: '800px',
+  paddingTop: '10px',
+  width: '100%',
 };
 
 const TicketDisplay = ({ tickets }) => (
-  <div style={divStyle} data-test="ticket-display">
+  <div style={divStyle} data-test="component-ticket-display">
     { tickets.map((ticket, idx) => (
       // eslint-disable-next-line react/no-array-index-key
       <Ticket ticket={ticket} key={`ticketNumber${idx}`} />
