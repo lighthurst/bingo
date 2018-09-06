8f9af5b9d92f6c3e3c0287fcfcb18879d539ab78
Aaron Tesfai <aaron.tesfai@gmail.com>
2018-09-05 21:21:56 -0700

    Add newgame and getBalls routes.

--- server/routes.js
+++ server/routes.js
@@ -4,5 +4,7 @@ const controller = require('./controller');
 // Connect controller methods to their corresponding routes
 router.get('/drawball', controller.drawBall.get);
 router.post('/verifytickets', controller.verifyTickets.post);
+router.get('/newgame', controller.newGame.get);
+router.get('/getballs', controller.getBalls.get);
 
 module.exports = router;
