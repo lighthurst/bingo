0528c26f161bbae6fe1dd3700c39b1201833e7d5
Aaron Tesfai <aaron.tesfai@gmail.com>
2018-09-06 13:29:11 -0700

    Reformat app file into two for testing purposes.

--- server/index.js
+++ server/index.js
@@ -1,19 +1,5 @@
-import express from 'express';
-import path from 'path';
-import gmRouter from './routesGM';
+import app from './app';
 
 const PORT = process.env.PORT || 3001;
-const PUBLIC_DIR = path.join(__dirname, '../public');
-const app = express();
 
-app
-  // Parse JSON payloads
-  .use(express.json())
-  // Serve the public files
-  .use(express.static(PUBLIC_DIR))
-  // Set up our routes
-  .use('/gm', gmRouter)
-  // Handle our errors
-  // .use(errorHandler)
-  // Listen to port and log
-  .listen(PORT, () => console.log(`Server listening on port ${PORT}`)); // eslint-disable-line no-console
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`)); // eslint-disable-line no-console
