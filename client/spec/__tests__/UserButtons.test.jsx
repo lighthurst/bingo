259841d44e7fe2de3edf1f04e8ac9c79cf39987f
Aaron Tesfai <aaron.tesfai@gmail.com>
2018-09-05 21:14:29 -0700

    Test rendering component UserButtons.

---
+++ client/spec/__tests__/UserButtons.test.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import UserButtons from '../../src/components/UserButtons';
+
+const setup = (props = {}, state = null) => {
+  // eslint-disable-next-line no-undef
+  const wrapper = shallow(<UserButtons {...props} />);
+  if (state) {
+    wrapper.setState(state);
+  }
+  return wrapper;
+};
+
+const findByTestAttr = (wrapper, val) => wrapper.find(`[data-test='${val}']`);
+
+test('render without error', () => {
+  const props = { drawBall() {}, verifyTickets() {} };
+  const wrapper = setup(props);
+  const userButtonsComponent = findByTestAttr(wrapper, 'component-user-buttons');
+  expect(userButtonsComponent.length).toBe(1);
+});
