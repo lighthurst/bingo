79dd5cc6f15a7ae67ba9f813a3f2d5576040d532
Aaron Tesfai <aaron.tesfai@gmail.com>
2018-09-05 21:26:34 -0700

    Implement verifyTickets, newGame, and getBalls; handle edge case if drawball is called when db size is full.

--- server/model/index.js
+++ server/model/index.js
@@ -3,14 +3,27 @@ const db = require('../db');
 module.exports = {
   drawBall: {
     get() {
-      const nextBall = Math.floor(Math.random() * 100) + 1;
+      let nextBall;
+      do {
+        nextBall = Math.floor(Math.random() * 100) + 1;
+      } while (db.has(nextBall) && db.size < 100);
       db.set(nextBall, true);
-      return Array.from(db.keys()).reverse(); // or .slice(-5)
+      return Array.from(db.keys());
     },
   },
   verifyTickets: {
     post(tickets) {
-      return tickets.some(ticket => ticket.every(row => row.every(number => db.has(number))));
+      return tickets.some(ticket => ticket.every(row => row.every(number => db.has(number.val))));
+    },
+  },
+  newGame: {
+    get() {
+      return db.clear();
+    },
+  },
+  getBalls: {
+    get() {
+      return Array.from(db.keys());
     },
   },
 };
