de08829bb33226dcbb9a82f98341bb9ec3694abd
Aaron Tesfai <aaron.tesfai@gmail.com>
2018-09-05 21:36:47 -0700

    Add isJumped and isWinner to state; implement markTicket, autoMarkTickets, and newGame clickhandler methods; implement getBalls and fetch on component mount; properly handle verifyTicket fetch; implement congratulateWinner method.

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,74 +1,72 @@
 import React, { Component } from 'react';
 import exampleTickets from '../../../assets/exampleData';
+import makeTicket from '../helpers';
 import GMDraws from './GMDraws';
 import UserButtons from './UserButtons';
 import TicketDisplay from './TicketDisplay';
 
 class App extends Component {
-  static makeTicket(maxRange) {
-    const ticket = [];
-    const bingoCols = {
-      0: 'b', 1: 'i', 2: 'n', 3: 'g', 4: 'o',
-    };
-    const randomNums = this.getRandomNums(25, 1, maxRange);
-    for (let i = 0; i < 5; i += 1) {
-      ticket.push([]);
-      for (let j = 0; j < 5; j += 1) {
-        ticket[i].push({
-          val: randomNums[i * 5 + j],
-          isMarked: false,
-          col: bingoCols[j],
-        });
-      }
-    }
-    return ticket;
-  }
-
   constructor() {
     super();
     this.state = {
       tickets: exampleTickets,
       recentBalls: [],
+      isJumped: false,
+      isWinner: false,
     };
     this.drawBall = this.drawBall.bind(this);
+    this.markTicket = this.markTicket.bind(this);
     this.verifyTickets = this.verifyTickets.bind(this);
+    this.newGame = this.newGame.bind(this);
+    this.autoMarkTickets = this.autoMarkTickets.bind(this);
   }
 
   componentDidMount() {
     this.makeTickets(4, 100);
-    this.drawBall();
+    this.getBalls();
   }
 
-  static getRandomNums(count, minRange, maxRange) {
-    const availNums = Array.from(new Array(maxRange), (val, idx) => idx + minRange);
-    let c = count;
-    let m = availNums.length;
-    while (c) {
-      const i = Math.floor(Math.random() * m);
-      m -= 1;
-      [availNums[m], availNums[i]] = [availNums[i], availNums[m]];
-      c -= 1;
-    }
-    return availNums.slice(-count);
+  getBalls() {
+    fetch('gm/getballs')
+      .then(response => response.json())
+      .then(recentBalls => this.setState({ recentBalls, isJumped: false }))
+      .catch(error => console.error('Error:', error)); // eslint-disable-line no-console
   }
 
   makeTickets(count, maxRange) {
     const tickets = [];
     for (let i = 0; i < count; i += 1) {
-      tickets.push(App.makeTicket(maxRange));
+      tickets.push(makeTicket(maxRange));
     }
     this.setState({ tickets });
+  }
+
+  newGame() {
+    fetch('gm/newgame')
+      .then(() => {
+        this.setState({ recentBalls: [], isJumped: false, isWinner: false });
+        this.makeTickets(4, 100);
+      })
+      .catch(error => console.error('Error:', error)); // eslint-disable-line no-console
   }
 
   drawBall() {
     fetch('gm/drawball')
       .then(response => response.json())
-      .then(recentBalls => this.markTickets(recentBalls))
+      .then(recentBalls => this.setState({ recentBalls, isJumped: false }))
       .catch(error => console.error('Error:', error)); // eslint-disable-line no-console
   }
 
-  markTickets(recentBalls) {
-    const { tickets } = this.state;
+  markTicket(ticketId, rowIdx, numberIdx, number) {
+    const { recentBalls, tickets } = this.state;
+    if (recentBalls.includes(number.val)) {
+      tickets[ticketId][rowIdx][numberIdx].isMarked = true;
+      this.setState({ tickets, isJumped: false });
+    }
+  }
+
+  autoMarkTickets() {
+    const { recentBalls, tickets } = this.state;
     tickets.forEach(ticket => ticket.forEach(row => row.forEach((number) => {
       if (recentBalls.includes(number.val)) {
         number.isMarked = true; // eslint-disable-line no-param-reassign
@@ -86,28 +84,38 @@ class App extends Component {
         'Content-Type': 'application/json',
       },
     }).then(res => res.json())
-      .then((isBingo) => {
-        console.log(isBingo); // eslint-disable-line no-console
-        if (isBingo) {
-          // this.congratulateWinner();
-        } else {
-          this.drawBall();
-        }
-      })
+      .then(isBingo => (isBingo ? this.congratulateWinner() : this.setState({ isJumped: true })))
       .catch(error => console.error('Error:', error)); // eslint-disable-line no-console
   }
 
-  // congratulateWinner() {
-
-  // }
+  congratulateWinner() {
+    this.setState(({ isWinner: true }));
+  }
 
   render() {
-    const { tickets, recentBalls } = this.state;
+    const {
+      tickets, recentBalls, isJumped, isWinner,
+    } = this.state;
     return (
       <div data-test="component-app">
-        <GMDraws recentBalls={recentBalls.slice(-5)} />
-        <UserButtons drawBall={this.drawBall} verifyTickets={this.verifyTickets} />
-        <TicketDisplay tickets={tickets} />
+        <GMDraws
+          recentBalls={recentBalls.slice(-5).reverse()}
+        />
+        <h1 style={{ textAlign: 'center' }}>
+          Bingo
+        </h1>
+        <UserButtons
+          drawBall={this.drawBall}
+          verifyTickets={this.verifyTickets}
+          newGame={this.newGame}
+          autoMarkTickets={this.autoMarkTickets}
+          isJumped={isJumped}
+          isWinner={isWinner}
+        />
+        <TicketDisplay
+          tickets={tickets}
+          markTicket={this.markTicket}
+        />
       </div>
     );
   }
