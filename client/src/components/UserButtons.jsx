688dc1fc3a2e507176c077391a1c90f92d00aa05
Aaron Tesfai <aaron.tesfai@gmail.com>
2018-09-05 21:32:38 -0700

    Add markAllDrawn and newGame buttons, rename verifyTickets button to callBingo, add popdowns if user jumpes the gun or wins.

--- client/src/components/UserButtons.jsx
+++ client/src/components/UserButtons.jsx
@@ -5,20 +5,40 @@ const divButtonsContainer = {
   paddingTop: '20px',
   width: '100%',
   textAlign: 'center',
+  display: 'flex',
+  flexwrap: 'wrap',
+  justifyContent: 'center',
+  alignItems: 'center',
 };
 
 const buttonDrawBallStyle = {
   display: 'block',
-  margin: '2px auto',
+  margin: '5px',
+  userSelect: 'none',
 };
 
 const buttonVerifyTicketsStyle = {
   display: 'block',
-  margin: '2px auto',
+  margin: '5px',
+  userSelect: 'none',
 };
 
-const UserButtons = ({ drawBall, verifyTickets }) => (
-  <div style={divButtonsContainer}>
+const buttonAutoMarkStyle = {
+  display: 'block',
+  margin: '5px',
+  userSelect: 'none',
+};
+
+const buttonNewGameStyle = {
+  display: 'block',
+  margin: '5px',
+  userSelect: 'none',
+};
+
+const UserButtons = ({
+  drawBall, verifyTickets, newGame, autoMarkTickets, isJumped, isWinner,
+}) => (
+  <div style={divButtonsContainer} data-test="component-user-buttons">
     <button
       type="button"
       style={buttonDrawBallStyle}
@@ -31,14 +51,44 @@ const UserButtons = ({ drawBall, verifyTickets }) => (
       style={buttonVerifyTicketsStyle}
       onClick={verifyTickets}
     >
-      Verify Tickets
+      Call Bingo
     </button>
+    <button
+      type="button"
+      style={buttonAutoMarkStyle}
+      onClick={autoMarkTickets}
+    >
+      Mark All Drawn
+    </button>
+    <button
+      type="button"
+      style={buttonNewGameStyle}
+      onClick={newGame}
+    >
+      New Game
+    </button>
+    {isJumped
+    && (
+      <div>
+        You jumped the gun. You need all numbers marked on a ticket to win.
+      </div>
+    )}
+    {isWinner
+    && (
+      <div>
+        Congratulations! You Won!
+      </div>
+    )}
   </div>
 );
 
 UserButtons.propTypes = {
   drawBall: PropTypes.func.isRequired,
   verifyTickets: PropTypes.func.isRequired,
+  autoMarkTickets: PropTypes.func.isRequired,
+  newGame: PropTypes.func.isRequired,
+  isJumped: PropTypes.bool.isRequired,
+  isWinner: PropTypes.bool.isRequired,
 };
 
 export default UserButtons;
